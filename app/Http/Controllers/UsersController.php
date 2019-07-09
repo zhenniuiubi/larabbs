d463e7af508cdf45940de0453a90571d90be5fb5
hehexia <minus1s@qq.com>
2019-07-09 22:40:43 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use App\Http\Requests\UserRequest;
 class UsersController extends Controller
 {
     //显示个人页面
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+
+    //显示个人页面
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request,User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success','个人资料更新成功!');
     }
 }
