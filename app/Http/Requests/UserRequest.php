7e89a09dc3a3d7186c5981fd225f0b62a4e1595c
hehexia <minus1s@qq.com>
2019-07-10 22:21:33 +0800

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,12 +28,15 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,gif,png,bmp|dimensions:min_width=208,min_height=208',
         ];
     }
 
     public function messages()
     {
         return [
+            'avatar.mimes' =>'头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 208px 以上',
             'name.unique' => '用户名 已被占用，请重新填写',
             'name.regex' => '用户名 只支持英文、数字、横杠和下划线。',
             'name.between' => '用户名 必须介于 3 - 25 个字符之间。',
