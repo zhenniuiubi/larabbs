33597892bd9e11959e276539c4087cdffdd24159
hehexia <minus1s@qq.com>
2019-07-19 14:34:19 +0800

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 //分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
